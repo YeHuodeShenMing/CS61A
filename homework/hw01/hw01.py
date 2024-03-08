32d1321e495793234198360dfb91a3d3d6efae31
liu-jiachen-2021301525 <ljc18392086470@163.com>
2024-03-08 17:30:14 +0800

    lab01

--- homework/hw01/hw01.py
+++ homework/hw01/hw01.py
@@ -1,5 +1,6 @@
 from operator import add, sub
 
+
 def a_plus_abs_b(a, b):
     """Return a+abs(b), but without calling abs.
 
@@ -13,10 +14,11 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
+
 
 def a_plus_abs_b_syntax_check():
     """Check that you didn't change the return statement of a_plus_abs_b.
@@ -42,7 +44,8 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i**2 + j**2 + k**2 - max(i, j, k) ** 2
+
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +69,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    for i in range(n // 2 + 1, 0, -1):
+        if n % i == 0:
+            return i
 
 
 def hailstone(n):
@@ -88,4 +94,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    step = 0
+    while n > 1:
+        print(n)
+        if n % 2 != 0:
+            n = n * 3 + 1
 
+        else:
+            n //= 2
+
+        step += 1
+    print("1")
+    return step + 1
--- test.py
+++ test.py
@@ -1,27 +1,12 @@
-import random
-
-def get_user_choice():
-    user_choice = input("请选择（石头/剪刀/布）：")
-    return user_choice.lower()
-
-def get_computer_choice():
-    choices = ["石头", "剪刀", "布"]
-    return random.choice(choices)
-
-def determine_winner(user, computer):
-    if user == computer:
-        return "平局！"
-    elif (user == "石头" and computer == "剪刀") or (user == "剪刀" and computer == "布") or (user == "布" and computer == "石头"):
-        return "你赢了！"
+def bake(cake, make):
+    if cake == 0:
+        cake = cake + 1
+        print(cake)
+    if cake == 1:
+        print(make)
     else:
-        return "计算机赢了！"
+        return cake
+    return make
 
-def main():
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    print(f"你选择了：{user_choice}")
-    print(f"计算机选择了：{computer_choice}")
-    print(determine_winner(user_choice, computer_choice))
 
-if __name__ == "__main__":
-    main()
+bake(0, 29)
