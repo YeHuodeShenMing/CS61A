7e2be59b2e0a3e194c608da95f1d494859cc3a27
liu-jiachen-2021301525 <ljc18392086470@163.com>
2024-03-09 17:46:02 +0800

    project1

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-def bake(cake, make):
-    if cake == 0:
-        cake = cake + 1
-        print(cake)
-    if cake == 1:
-        print(make)
-    else:
-        return cake
-    return make
-
-
-bake(0, 29)
+print(121 // 10, 121 % 10)
+print(2 // 10, 2 % 10)
