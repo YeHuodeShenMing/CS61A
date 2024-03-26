1ddefeef3def5067e025ddb50c9c0128939b5839
liu-jiachen-2021301525 <ljc18392086470@163.com>
2024-03-26 23:48:59 +0800

    2024-03-26

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
-GOAL = 100
-
-
-def main():
-    is_always_roll(always_roll_5)
-
-
-def is_always_roll(strategy, goal=GOAL):
-    num_rolls_list = []
-
-    def strategy(num_rolls, score, opponent_score):
-        while score < goal and opponent_score < goal:
-            num_rolls_list.append(num_rolls)
-        test_digit = num_rolls_list[0]
-        for num in num_rolls_list:
-            if test_digit != num:
-                return False
-
-    return strategy
-
-def always_roll_5(score, opponent_score):
-    """A strategy of always rolling 5 dice, regardless of the player's score or
-    the opponent's score.
-    """
-    return 5
-
-main()
+bi, bj, pi, pj = map(int, input().split())
+while bi >= 0 and bj >= 0 and pi >= 0 and pj >= 0:
+    for i in range(bi):
+        for j in range(bj):
+            if i == pi and  j == pj:
+                
+                
+    bi, bj, pi, pj = map(int, input().split())
